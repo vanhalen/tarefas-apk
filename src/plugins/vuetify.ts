42753772abfe84c11e15c8f8e3e8e7ee754e7759
Rodrigo Chagas <rodrigorrchagas@gmail.com>
2021-02-11 17:31:56 -0300

    chore: configurando cores e nome do apk

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,20 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#f44336",
+        secondary: "#e91e63",
+        accent: "#3f51b5",
+        error: "#8bc34a",
+        warning: "#607d8b",
+        info: "#795548",
+        success: "#ff5722",
+      },
+    },
+  },
 });
