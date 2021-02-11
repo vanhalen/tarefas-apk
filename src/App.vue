94076cb9321576c44275e079653958935ae67660
Rodrigo Chagas <rodrigorrchagas@gmail.com>
2021-02-11 17:32:29 -0300

    chore: chamando novos componentes

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,37 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-      <v-btn @click="componente = 'HelloWorld'" text>
-        Início
-      </v-btn>
-      <v-btn @click="componente = 'MyComponent'" text>
-        Produtos
-      </v-btn>
+      <p>R. Henrique Casela, 58</p>
     </v-app-bar>
-
     <v-main>
       <component :is="componente"> </component>
     </v-main>
+
+    <Menu @componente="componente = $event" />
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
-import MyComponent from "./components/MyComponent.vue";
+import Menu from "./components/Menu.vue";
+import Inicio from "./views/Inicio.vue";
+import Busca from "./views/Busca.vue";
+import Pedidos from "./views/Pedidos.vue";
+import Perfil from "./views/Perfil.vue";
+
 export default Vue.extend({
   name: "App",
 
   components: {
-    HelloWorld,
-    MyComponent,
+    Menu,
+    Inicio,
+    Busca,
+    Pedidos,
+    Perfil,
   },
 
   data: () => ({
-    componente: "HelloWorld",
+    componente: "Inicio",
   }),
 });
 </script>
